1b0c7d6f0434fd0c2568b398111b0d1593c9a706
Luis Diego Cordero Hernandez <lcordero@growthaccelerationpartners.com>
2015-08-27 23:01:41 -0600

    MEAN Book add project files

---
+++ gulp/server.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp')
+var nodemon = require('gulp-nodemon')
+
+var gulp = require('gulp')
+var nodemon = require('gulp-nodemon')
+
+gulp.task('dev:server', function () {
+    nodemon({
+        script: 'server.js',
+        ext:    'js',
+        ignore: ['ng*', 'gulp*', 'assets*']
+    })
+})
